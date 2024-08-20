1e50107210b7675175f93195d16495a9805ca49e
evelyne-phich <evelyne.phich@gmail.com>
2024-08-20 16:47:29 +0200

    add redirection to home page in navbar

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,8 +1,10 @@
+import { Stack } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   return (
@@ -17,16 +19,20 @@ export const NavBar = () => {
     >
       <AppBar position="static" sx={{ flexGrow: 1, backgroundColor: red[900] }}>
         <Toolbar sx={{ gap: 2 }}>
-          <img
-            src="/assets/images/cook.svg"
-            alt="Cheffe cuisinière"
-            loading="lazy"
-            width={36}
-            height={36}
-          />
-          <Typography variant="h6" component="p" sx={{ flexGrow: 1 }}>
-            On cuisine quoi ?
-          </Typography>
+          <Link to="/">
+            <Stack flexDirection="row" alignItems="center" gap={1}>
+              <img
+                src="/assets/images/cook.svg"
+                alt="Cheffe cuisinière"
+                loading="lazy"
+                width={36}
+                height={36}
+              />
+              <Typography variant="h6" component="p" sx={{ flexGrow: 1 }}>
+                On cuisine quoi ?
+              </Typography>
+            </Stack>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
