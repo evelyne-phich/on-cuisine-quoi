5bf85eee3aa174e0856efceafed8561cfad58836
evelyne-phich <evelyne.phich@gmail.com>
2024-08-21 17:37:40 +0200

    add scroll to top when changing route

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,16 @@
 import { Stack } from "@mui/material";
 import "../../index.scss";
 import { NavBar } from "src/components/layout/NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export const Layout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <Stack paddingTop={8}>
       <NavBar />
